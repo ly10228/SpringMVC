a8db89fcd36b2cfa83723d9c563023c707c2460c
ly10228 <601520157@qq.com>
2020-01-29 13:34:08 +0800

    名字默认初始化

--- src/main/java/com/ly/springmvc/ss/beans/Person.java
+++ src/main/java/com/ly/springmvc/ss/beans/Person.java
@@ -1,7 +1,7 @@
 package com.ly.springmvc.ss.beans;
 
 public class Person {
-	private String name ="SpringMvc+Springz";
+	private String name ="SpringMvc+Springzz";
 
 	public String getName() {
 		return name;
